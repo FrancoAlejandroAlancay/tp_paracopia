ef0454b7690ee4acc8c2929642b6457cbe8b7b21
Franco Alejandro Alancay <francoalancayomg369@gmail.com>
2022-05-12 02:57:06 -0300

    first commit

---
+++ src/test/java/ar/edu/unju/fi/tp_paracopia/TpParacopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp_paracopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpParacopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
